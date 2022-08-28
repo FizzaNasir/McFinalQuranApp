964146f6bf248d177265f67b348b3c239d43723e
Fizza <fizzanasir976@gmail.com>
2022-08-29 01:32:03 +0500

    home page completed

--- app/src/main/java/com/fizzanasir/mcfinalproject/MainActivity.java
+++ app/src/main/java/com/fizzanasir/mcfinalproject/MainActivity.java
@@ -1,14 +1,44 @@
 package com.fizzanasir.mcfinalproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    private Button Viewpara;
+    private Button Viewsurah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Viewpara = (Button)findViewById(R.id.showpara);
+        Viewsurah = (Button)findViewById(R.id.showsurah);
+
+        Viewsurah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(MainActivity.this, SurahList.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
